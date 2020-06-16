7bbb6f48870bf5406ea406de69d1dcebd8beccfe
Juliane Fink <jule-fink@gmx.de>
2020-06-16 11:00:50 +0200

    update board, cleanup

--- tairait/src/BoardManager.java
+++ tairait/src/BoardManager.java
@@ -1,8 +1,34 @@
+import lenz.htw.tiarait.net.NetworkClient;
+
 public class BoardManager {
 
-    public int[][] board = new int[32][32];
+    /*
+        Codes
+        -1 = obstacle
+         0 = empty
+         1 = team 0
+         2 = team 1
+         3 = team 2
+         4 = team 3
+     */
+    private int[][] board = new int[32][32];
 
-    public void setObstacle(int x, int y){
-        board[x][y] = -1;
+    public void setObstacles(NetworkClient client){
+        int counter = 0;
+        for(int y = 0; y < board.length; y++){
+            for(int x = 0; x < board[y].length; x++){
+                if(client.isWall(x,y)){
+                    board[y][x] = -1;
+                    counter++;
+
+                }
+            }
+        }
+        System.out.println("Found " + counter + " obstacles.");
+    }
+
+    public void updateBoard(int x, int y, int code) {
+        board[y][x] = code;
+        System.out.println("Updated board: " + code);
     }
 }
--- tairait/src/Client.java
+++ tairait/src/Client.java
@@ -5,28 +5,21 @@ import lenz.htw.tiarait.net.NetworkClient;
 
 import java.io.IOException;
 
-public class Client {
+public class Client implements Runnable {
+    private String name;
 
-    //spieler werden im mittleren drittel gespawnt
-    //space starten
-    public static void main(String[] args) throws IOException {
+    public Client(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
         BoardManager boardManager = new BoardManager();
-        NetworkClient nc =  new NetworkClient(null, "teamName");
+        NetworkClient nc =  new NetworkClient(null, name);
         int playerNumber = nc.getMyPlayerNumber(); //0 = rot, 1 = grün, 2=blau, 3=gelb
+        boardManager.setObstacles(nc);
 
-        int counter = 0;
-        for(int i = 0; i < boardManager.board.length; i++){
-            for(int j = 0; j < boardManager.board[i].length; j++){
-                if(nc.isWall(i,j)){
-                    boardManager.setObstacle(i, j);
-                    counter++;
-                    System.out.println("found obstacle: " + counter);
-                }
-            }
-        }
         while(nc.isAlive()){
-            //steuerung
-            ;
             int botNr = 0; //0-3
             float x = nc.getX(playerNumber, botNr);
             float y = nc.getY(playerNumber, botNr);
@@ -37,14 +30,26 @@ public class Client {
             nc.setMoveDirection(2, 1,1);
             // Latenzen
             // kein Bremsen, keine Beschleunigung
+
             ColorChange cc;
-            /*while((cc=nc.getNextColorChange()) != null){
+            while((cc = nc.getNextColorChange()) != null){
                 //liste, iterator
                 //cc in eigene struktur einarbeiten
                 //cc.newColor; //o=leer; 1-4 spieler //nur einmalig abfragbar
+                boardManager.updateBoard(cc.x, cc.y, cc.newColor);
                 //punkte zählen
-            }*/
+            }
         }
+    }
+
+    //spieler werden im mittleren drittel gespawnt
+    //space starten
+    public static void main(String[] args) throws IOException {
+
+            new Thread(new Client("A")).start();
+            new Thread(new Client("B")).start();
+            new Thread(new Client("C")).start();
+            new Thread(new Client("D")).start();
 
     }
 }
